aeea5c515f1e2eadb2ae2f911757bf7093f45381
Koen Teuwen <k.t.w.teuwen@tue.nl>
2024-12-18 17:22:57 +0100

    fix

--- docs/static/js/umami.js
+++ docs/static/js/umami.js
@@ -1,1 +1,9 @@
-<script async="" src="https://umami.suricata-check.teuwen.net/script.js" data-website-id="122e579a-0a3b-41c2-8b20-9a3cbec8dc76" data-do-not-track="true" data-domains="suricata-check.teuwen.net"></script>
+var script = document.createElement("script");
+
+script.setAttribute("async", "");
+script.setAttribute("src", "https://umami.suricata-check.teuwen.net/script.js");
+script.setAttribute("data-website-id", "122e579a-0a3b-41c2-8b20-9a3cbec8dc76");
+script.setAttribute("data-do-not-track", "true");
+script.setAttribute("data-domains", "suricata-check.teuwen.net");
+
+document.body.appendChild(script);
